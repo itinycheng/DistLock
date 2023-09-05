cc1b7dec17c1b5c83f9bc4973930d898b8f649bb
chenglong.wang <tiny.wcl@gmail.com>
2023-09-05 15:50:45 +0800

    feat: diesel r2d2

--- src/provider/diesel.rs
+++ src/provider/diesel.rs
@@ -3,7 +3,6 @@ use std::fmt::Display;
 use chrono::Utc;
 use diesel::sql_types::BigInt;
 use diesel::sql_types::VarChar;
-use diesel::MysqlConnection;
 use diesel::QueryableByName;
 use diesel::RunQueryDsl;
 use gethostname::gethostname;
@@ -70,63 +69,94 @@ impl<T> DieselDriver<T> {
 	}
 }
 
-impl Lockable for DieselDriver<MysqlConnection> {
-	fn acquire_lock(&mut self, config: &LockConfig) -> LockResult<LockState> {
-		let now = Utc::now();
-		let until = now + config.max_lock;
+macro_rules! impl_lockable_diesel {
+	(
+		$client: ty,
+		$self: ident,
+		$conn: expr
+	) => {
+		impl Lockable for DieselDriver<$client> {
+			fn acquire_lock(&mut $self, config: &LockConfig) -> LockResult<LockState> {
+				let now = Utc::now();
+				let until = now + config.max_lock;
 
-		let locked = match diesel::sql_query(format!(
-			"INSERT INTO {} (name, lock_until, locked_at, locked_by) VALUES (?, ?, ?, ?)",
-			&self.table
-		))
-		.bind::<VarChar, _>(&config.name)
-		.bind::<BigInt, _>(until.timestamp_millis())
-		.bind::<BigInt, _>(now.timestamp_millis())
-		.bind::<VarChar, _>(gethostname().to_string_lossy())
-		.execute(&mut self.transport)
-		{
-			Ok(count) if count > 0 => true,
-			_ => {
-				match diesel::sql_query(format!(
-				"UPDATE {} SET lock_until = ?, locked_at = ?, locked_by = ? WHERE name = ? AND lock_until <= ?",
-			 &self.table))
+				let mut locked = match diesel::sql_query(format!(
+					"INSERT INTO {} (name, lock_until, locked_at, locked_by) VALUES (?, ?, ?, ?)",
+					&$self.table
+				))
+				.bind::<VarChar, _>(&config.name)
 				.bind::<BigInt, _>(until.timestamp_millis())
 				.bind::<BigInt, _>(now.timestamp_millis())
 				.bind::<VarChar, _>(gethostname().to_string_lossy())
-				.bind::<VarChar, _>(&self.name)
-				.bind::<BigInt, _>(until.timestamp_millis())
-				.execute(&mut self.transport)
+				.execute($conn)
 				{
-					Ok(count) => count > 0,
-					Err(_) => false,
+					Ok(count)  => count > 0,
+					Err(_) => false
+				};
+
+				if !locked {
+					locked = diesel::sql_query(format!(
+						"UPDATE {} SET lock_until = ?, locked_at = ?, locked_by = ? WHERE name = ? AND lock_until <= ?",
+					 &$self.table))
+						.bind::<BigInt, _>(until.timestamp_millis())
+						.bind::<BigInt, _>(now.timestamp_millis())
+						.bind::<VarChar, _>(gethostname().to_string_lossy())
+						.bind::<VarChar, _>(&$self.name)
+						.bind::<BigInt, _>(until.timestamp_millis())
+						.execute($conn)? > 0;
 				}
+
+				Ok(LockState::new(locked, Utc::now()))
 			}
-		};
 
-		Ok(LockState::new(locked, Utc::now()))
-	}
+			fn release_lock(&mut $self, config: &LockConfig, state: &LockState) -> LockResult<LockState> {
+				let lock_until = config.lock_at_least_until(state.locked_at);
+				diesel::sql_query(format!("UPDATE {} SET lock_until = ? WHERE name = ?", &$self.table))
+					.bind::<BigInt, _>(lock_until.timestamp_millis())
+					.bind::<VarChar, _>(&$self.name)
+					.execute($conn)?;
+				Ok(LockState::new(false, Utc::now()))
+			}
 
-	fn release_lock(&mut self, config: &LockConfig, state: &LockState) -> LockResult<LockState> {
-		let lock_until = config.lock_at_least_until(state.locked_at);
-		diesel::sql_query(format!("UPDATE {} SET lock_until = ? WHERE name = ?", &self.table))
-			.bind::<BigInt, _>(lock_until.timestamp_millis())
-			.bind::<VarChar, _>(&self.name)
-			.execute(&mut self.transport)?;
-		Ok(LockState::new(false, Utc::now()))
-	}
-
-	fn extend_lock(&mut self, config: &LockConfig) -> LockResult<LockState> {
-		let now = Utc::now();
-		let until = now + config.max_lock;
-		let count = diesel::sql_query(format!(
-			"UPDATE {} SET lock_until = ? WHERE name = ? AND locked_by = ? AND lock_until > ?",
-			&self.table
-		))
-		.bind::<BigInt, _>(until.timestamp_millis())
-		.bind::<VarChar, _>(&self.name)
-		.bind::<VarChar, _>(gethostname().to_string_lossy())
-		.bind::<BigInt, _>(now.timestamp_millis())
-		.execute(&mut self.transport)?;
-		Ok(LockState::new(count > 0, Utc::now()))
-	}
+			fn extend_lock(&mut $self, config: &LockConfig) -> LockResult<LockState> {
+				let now = Utc::now();
+				let until = now + config.max_lock;
+				let count = diesel::sql_query(format!(
+					"UPDATE {} SET lock_until = ? WHERE name = ? AND locked_by = ? AND lock_until > ?",
+					&$self.table
+				))
+				.bind::<BigInt, _>(until.timestamp_millis())
+				.bind::<VarChar, _>(&$self.name)
+				.bind::<VarChar, _>(gethostname().to_string_lossy())
+				.bind::<BigInt, _>(now.timestamp_millis())
+				.execute($conn)?;
+				Ok(LockState::new(count > 0, Utc::now()))
+			}
+		}
+	};
 }
+
+#[cfg(feature = "diesel_sqlite")]
+impl_lockable_diesel!(::diesel::SqliteConnection, self, &mut self.transport);
+#[cfg(feature = "diesel_postgres")]
+impl_lockable_diesel!(::diesel::PgConnection, self, &mut self.transport);
+#[cfg(feature = "diesel_mysql")]
+impl_lockable_diesel!(::diesel::MysqlConnection, self, &mut self.transport);
+#[cfg(feature = "diesel_sqlite_r2d2")]
+impl_lockable_diesel!(
+	::r2d2::Pool<::diesel::r2d2::ConnectionManager<::diesel::SqliteConnection>>,
+	self,
+	&mut self.transport.get()?
+);
+#[cfg(feature = "diesel_postgres_r2d2")]
+impl_lockable_diesel!(
+	::r2d2::Pool<::diesel::r2d2::ConnectionManager<::diesel::PgConnection>>,
+	self,
+	&mut self.transport.get()?
+);
+#[cfg(feature = "diesel_mysql_r2d2")]
+impl_lockable_diesel!(
+	::r2d2::Pool<::diesel::r2d2::ConnectionManager<::diesel::MysqlConnection>>,
+	self,
+	&mut self.transport.get()?
+);
--- tests/diesel_test.rs
+++ tests/diesel_test.rs
@@ -20,19 +20,39 @@ mod diesel {
 	use diesel::MysqlConnection;
 	use dist_lock::core::DistLock;
 	use dist_lock::core::LockConfig;
+	use dist_lock::core::Lockable;
 	use dist_lock::error::LockResult;
 	use dist_lock::provider::diesel::DieselDriver;
 
+	#[cfg(feature = "diesel_mysql")]
 	#[test]
-	fn test_lock() -> LockResult<()> {
-		let now = Instant::now();
+	fn test_mysql_lock() -> LockResult<()> {
 		let db_url = "mysql://root:123456@127.0.0.1:3306/diesel_test";
 		let conn = MysqlConnection::establish(db_url)?;
 		let lock_name = "random_lock".to_string();
 		let driver = DieselDriver::new(&lock_name, Some("t"), conn);
 		let config = LockConfig::new(lock_name, Duration::seconds(0), Duration::seconds(10));
 		let mut dist_lock = DistLock::new(config, driver);
+		check_lock(&mut dist_lock)
+	}
 
+	#[cfg(feature = "diesel_mysql_r2d2")]
+	#[test]
+	fn test_r2d2_mysql_lock() -> LockResult<()> {
+		use diesel::r2d2::ConnectionManager;
+		use r2d2::Pool;
+		let db_url = "mysql://root:123456@127.0.0.1:3306/diesel_test";
+		let manager = ConnectionManager::<MysqlConnection>::new(db_url);
+		let pool = Pool::builder().max_size(1).test_on_check_out(true).build(manager)?;
+		let lock_name = "random_lock".to_string();
+		let driver = DieselDriver::new(&lock_name, Some("t"), pool);
+		let config = LockConfig::new(lock_name, Duration::seconds(0), Duration::seconds(10));
+		let mut dist_lock = DistLock::new(config, driver);
+		check_lock(&mut dist_lock)
+	}
+
+	fn check_lock<T: Lockable>(dist_lock: &mut DistLock<T>) -> LockResult<()> {
+		let now = Instant::now();
 		assert!(dist_lock.acquire()?);
 		thread::sleep(core::time::Duration::from_secs(5));
 		assert!(dist_lock.extend()?);
